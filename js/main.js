e7a607355a5edec147b723705dec232f584e87e8
Tarit Goswami <taritgoswami456@gmail.com>
2020-08-30 22:55:11 +0530

    adding weather in contact section

--- js/main.js
+++ js/main.js
@@ -159,3 +159,87 @@
   });
 
 })(jQuery);
+
+// Contact/For the weather section
+window.addEventListener('load', ()=> {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector(".temperature-description");
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector(".location-timezone");
+  const temperaturSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position =>{
+      // console.log(position);
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      // const proxy = 'https://cors-anywhere.herokuapp.com';
+      const apiquery = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+      // fetch the weather data according to the link: https://rapidapi.com/weatherbit/api/weather 
+      fetch(apiquery)
+      .then(response => {
+        
+        return response.json();
+      })
+      .then(data => {
+        // console.log(data);
+        const {temperature, summary, icon} = data.currently;
+        // Set DOM Elements from API
+        temperatureDegree.textContent = temperature;
+        temperatureDescription.textContent = summary;
+        locationTimezone.textContent = data.timeZone;
+        let celcius = (temperature -32) * (9 / 5);
+        // Setting icon for weather
+        setIcons(icon, document.querySelector(".icon"));
+
+        // Change temprature to ferenhite when clicked
+        temperaturSection.addEventListener("click", ()=> {
+          if(temperatureSpan.textContent === "F") {
+            temperatureSpan.textContent = "C";
+            temperatureDegree.textContent = Math.floor(celcius);
+          } else {
+            temperatureSpan.textContent = "F";
+            temperatureDegree.textContent = temperature;
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    });
+  }
+
+  function setIcons(icon, iconID) {
+    const skycons = new Skycons({color: "white"});
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
